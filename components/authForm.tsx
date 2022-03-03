428ad6f855e53223194ab811b665376d7bd8454c
whatapalaver <angela@sixtostart.com>
2022-03-03 15:28:50 +0000

    earlier

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,12 +1,11 @@
-import {} from '@chakra-ui/layout'
-import { Box, Flex, Input, Button} from '@chakra-ui/react';
-import { useRouter } from 'next/router';
-import { useSWRConfig } from 'swr';
-import { auth } from 'lib/mutations';
-import {FC, useState} from 'react';
-import NextImage from 'next/image';
+import { Box, Flex, Input, Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { FC, useState } from 'react'
+import { useSWRConfig } from 'swr'
+import NextImage from 'next/image'
+import { auth } from '../lib/mutations'
 
-const AuthForm: FC<{mode: 'signin' | 'signup'}> = ({mode}) => {
+const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [isLoading, setIsLoading] = useState(false)
@@ -16,35 +15,44 @@ const AuthForm: FC<{mode: 'signin' | 'signup'}> = ({mode}) => {
     e.preventDefault()
     setIsLoading(true)
 
-    const user = await auth(mode, { email, password })
+    await auth(mode, { email, password })
     setIsLoading(false)
     router.push('/')
   }
 
   return (
     <Box height="100vh" width="100vw" bg="black" color="white">
-      <Flex justify="center" align = "center" height="100px" borderBottom="white 1px solid">
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
         <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
-      <Flex justify="center" align = "center" height="calc(100vh - 100px)">
+      <Flex justify="center" align="center" height="calc(100vh - 100px)">
         <Box padding="50px" bg="gray.900" borderRadius="6px">
           <form onSubmit={handleSubmit}>
-            <Input 
+            <Input
               placeholder="email"
               type="email"
               onChange={(e) => setEmail(e.target.value)}
-              marginBottom="5px"
             />
-            <Input 
+            <Input
               placeholder="password"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
-              marginBottom="5px"
             />
-            <Button type="submit" bg="green.500" isLoading={isLoading} sx={{'&:hover': {
-              bg: 'green.300',
-            },
-            }}>
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                '&:hover': {
+                  bg: 'green.300',
+                },
+              }}
+            >
               {mode}
             </Button>
           </form>
