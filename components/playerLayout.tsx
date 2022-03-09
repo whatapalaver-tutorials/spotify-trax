7c56219323856f4857409a01ebbd415ee76d12ab
whatapalaver <angela@sixtostart.com>
2022-03-09 13:56:40 +0000

    Started profile

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -6,7 +6,9 @@ export default function PlayerLayout({children}) {
       <Box position="absolute" top="0" width="250px" left="0">
         <Sidebar />
       </Box>
-      <Box marginLeft="250" marginBottom="100px" > {children}</Box>
+      <Box marginLeft="250" marginBottom="100px" > 
+        <Box height="calc(100vh - 100px)">{children}</Box>
+      </Box>
       <Box width="100%" height="100px" position="absolute" left="0" bottom="0" bg="green">
         player
       </Box>
